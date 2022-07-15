88d779526e1696e7a4c892589221415d56274882
Nadira Raihanah <nadira.ihanah@gmail.com>
2022-07-15 10:30:45 +0700

    refactor: integrate data on dashboard admin to database
    - integrate data on dashboard admin to database
    - fix slicing on dashboard admin
    - update the order of data on database event on dashboard admin

--- resources/views/pages/dashboardAdmin.blade.php
+++ resources/views/pages/dashboardAdmin.blade.php
@@ -27,12 +27,7 @@
                     <div class="white-box analytics-info">
                         <h3 class="box-title">Total Users</h3>
                         <ul class="list-inline two-part d-flex align-items-center mb-0">
-                            <li>
-                                <div id="sparklinedash"><canvas width="67" height="30"
-                                        style="display: inline-block; width: 67px; height: 30px; vertical-align: top;"></canvas>
-                                </div>
-                            </li>
-                            <li class="ms-auto"><span class="counter text-success">100</span></li>
+                            <li class="ms-auto"><span class="counter text-success">{{ $user }}</span></li>
                         </ul>
                     </div>
                 </div>
@@ -40,12 +35,7 @@
                     <div class="white-box analytics-info">
                         <h3 class="box-title">Total Events</h3>
                         <ul class="list-inline two-part d-flex align-items-center mb-0">
-                            <li>
-                                <div id="sparklinedash2"><canvas width="67" height="30"
-                                        style="display: inline-block; width: 67px; height: 30px; vertical-align: top;"></canvas>
-                                </div>
-                            </li>
-                            <li class="ms-auto"><span class="counter text-purple">550</span></li>
+                            <li class="ms-auto"><span class="counter text-purple">{{ $event }}</span></li>
                         </ul>
                     </div>
                 </div>
